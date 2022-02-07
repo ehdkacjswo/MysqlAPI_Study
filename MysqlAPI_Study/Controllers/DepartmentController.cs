43a5dc30f4521108b67c053715f30d55b1973c97
ehdka <ehdka@DESKTOP-69IGIF8>
2022-02-07 22:50:44 +0900

    초기 커밋.

---
+++ MysqlAPI_Study/Controllers/DepartmentController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using MysqlAPI_Study.Models;
+using System.Data;
+#nullable disable
+namespace MysqlAPI_Study.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<Department> Get()
+        {
+            string query = @"select DepartmentId, DepartmentName from mytestdb.Department";
+            
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand myCommand=new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    return table.AsEnumerable().Select(row =>
+                    new Department
+                    {
+                        DepartmentID = row.Field<int>("DepartmentId"),
+                        DepartmentName = row.Field<string>("DepartmentName")
+                    }) ;
+                }
+            }
+        }
+
+        
+    }
+}
