d7bd630676ac4c26c2c81d7dd8db1fb442a21a89
Doam Lee <ehdkacjswo@gmail.com>
2022-02-09 14:13:27 +0900

    Update added

--- MysqlAPI_Study/Controllers/DepartmentController.cs
+++ MysqlAPI_Study/Controllers/DepartmentController.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient;
-using MysqlAPI_Study.Models;
 using System.Data;
+using MysqlAPI_Study.Models;
+
+// Mysql namespace
+using MySql.Data.MySqlClient;
+
+// Fluentd namespace
+using Serilog;
+using Serilog.Sinks.Fluentd;
+
+
+
 #nullable disable
 namespace MysqlAPI_Study.Controllers
 {
@@ -11,8 +20,11 @@ namespace MysqlAPI_Study.Controllers
     public class DepartmentController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        Serilog.Core.Logger log;
+
         public DepartmentController(IConfiguration configuration)
         {
+            log = new LoggerConfiguration().WriteTo.Fluentd().CreateLogger();
             _configuration = configuration;
         }
 
@@ -27,6 +39,7 @@ namespace MysqlAPI_Study.Controllers
             using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
             {
                 mycon.Open();
+                log.Information("Get Succeed");
                 using(MySqlCommand myCommand=new MySqlCommand(query, mycon))
                 {
                     myReader = myCommand.ExecuteReader();
@@ -58,10 +71,45 @@ namespace MysqlAPI_Study.Controllers
                 {
                     if (myCommand.ExecuteNonQuery() == 1)
                     {
+                        log.Information($"Put Succeed");
                         return new JsonResult("Addition Succeed");
                     }
 
-                    else return new JsonResult("Addition Failed");
+                    else
+                    {
+                        log.Information($"Put Failed");
+                        return new JsonResult("Addition Failed");
+                    }
+                }
+            }
+        }
+
+        [HttpPatch]
+        public JsonResult Patch(Department dep)
+        {
+            string query = $"update mytestdb.Department set DepartmentName = \"{dep.DepartmentName}\"" +
+                $"where DepartmentId = {dep.DepartmentID}";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    if (myCommand.ExecuteNonQuery() == 1)
+                    {
+                        log.Information($"Put Succeed");
+                        return new JsonResult("Addition Succeed");
+                    }
+
+                    else
+                    {
+                        log.Information($"Put Failed");
+                        return new JsonResult("Addition Failed");
+                    }
                 }
             }
         }
